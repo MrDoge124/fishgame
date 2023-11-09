2bf7b9cbb40d2e56dcea5af7e2daa2a4412038ed
Joseph <joseph.galvin@blueyonder.co.uk>
2023-11-09 17:23:12 +0000

    Added spawn time scaling and player can move upwards

--- Assets/Scripts/TrashSpawner.cs
+++ Assets/Scripts/TrashSpawner.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class TrashSpawner : MonoBehaviour
 {
     [SerializeField]GameObject[] Trash;
-    float movetimerMax = 4;
+    public float movetimerMax = 5;
     float movetimer;
     float maxXpos = 21;
     float minXpos = -21;
+    GameManager gm;
     // Start is called before the first frame update
     void Start()
     {
+        gm = GetComponent<GameManager>();
         transform.position = new Vector3(0, 12, 0);
     }
 
@@ -24,6 +27,8 @@ public class TrashSpawner : MonoBehaviour
             movetimer = 0;
             Instantiate(Trash[Random.Range(0, Trash.Length)], this.transform.position, this.transform.rotation);
             transform.position = new Vector3(Random.Range(minXpos, maxXpos), 12, 0);
+            movetimerMax = 5 - gm.Score / 100 * 5;
+            movetimerMax = Mathf.Clamp(movetimerMax, 2.2f, 100);
         }
     }
 }
