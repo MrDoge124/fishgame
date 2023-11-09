5b8c501e2766e7921cd985d1f8dfe9f349ce023f
Joseph <joseph.galvin@blueyonder.co.uk>
2023-11-09 15:16:33 +0000

    Added game over menu and ported pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,16 +10,29 @@ public class GameManager : MonoBehaviour
     int Lives;
     [SerializeField]TextMeshProUGUI scoreText;
     [SerializeField]TextMeshProUGUI livesText;
+    public bool gameOver;
+    [SerializeField]GameObject gameoverUI;
     // Start is called before the first frame update
     void Start()
     {
         Lives = 3;
         Score = 0;
+        gameOver = false;
+        gameoverUI.SetActive(false);
+        Time.timeScale = 1f;
     }
+    #region GameStates
     public void EndGame() 
     {
+        gameoverUI.SetActive(true);
+        gameOver = true;
         Time.timeScale = 0;
     }
+    public void ChangeScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+    #endregion
     #region score
     public void AddScore(int scoretoadd)
     {
